3439b4f2c17c22e956d66a584ea770f1398b0677
sgsjha <sarthakjhaa11@gmail.com>
2024-11-13 03:21:29 +0000

    Name change lol

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,12 +29,12 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10 ">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dyanmic Web Magic with Next.JS
+            Portfolio Website built with React,Next JS and Tailwind CSS
           </h2>
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforing Concepts into Seamless User Experiences"
+            words="SARTHAK JHA"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
