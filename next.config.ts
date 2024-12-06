4441d11b688760578bbc0ce8bd9ba428b3b28bcb
sgsjha <sarthakjhaa11@gmail.com>
2024-12-06 18:01:16 +0000

    local host works now

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,8 @@ const nextConfig: NextConfig = {
   
 };
 
-const isProd = process.env.NODE_ENV === 'production';
+//const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  output: 'export', // Ensure it's static
-  basePath: '/Portfolio-Website', // Replace <repository-name> with your repo name
-  assetPrefix: isProd ? '/Portfolio-Website' : '',
-};
+
 
 export default nextConfig;
